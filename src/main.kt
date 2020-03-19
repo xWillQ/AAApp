a4c6a43f5ad0d1556b780dc592dc40c95855cd55
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-19 19:30:52 +0700

    Completed plan item 6.3

--- src/main.kt
+++ src/main.kt
@@ -2,9 +2,16 @@ import kotlin.system.exitProcess
 
 fun printHelp() {
     println("Usage: app.jar [-h] [-login <login> -pass <pass> [-res <str> -role <str> [-ds <yyyy-mm-dd> -de <yyyy-mm-dd> -vol <int>] ] ]")
-    exitProcess(1)
 }
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) exitProcess(0)
+    if (args.isEmpty()) {
+        printHelp()
+        exitProcess(1)
+    }
+    if (args[0] == "-h") {
+        printHelp()
+        exitProcess(1)
+    }
+
 }
