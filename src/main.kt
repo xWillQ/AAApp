22f245d66a69e0ed025dda8910a5061345d3699c
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-20 21:31:01 +0700

    Completed plan items 11.5 and 11.6

--- src/main.kt
+++ src/main.kt
@@ -5,7 +5,10 @@ data class Permission(val res: String, val role: String, val user: User)
 data class Activity(val user: User, val res: String, val role: String, val ds: String, val de: String, val vol: Int)
 
 
-val users = listOf(User("vasya", "123"), User("admin", "admin"), User("q", "?!#"), User("abcdefghij", "qwerty"))
+val users = listOf(User("vasya", "123", "iYqHUi2<2zPhrGIL8]?p8m;bteA?ETaT", "dc6a8709e9fc8de1acea34fdc98c842911686ca0c2a0b12127c512a5ed7ab382"),
+        User("admin", "admin", "olMMIDct3GkrY:?Xp1WDJOPTw2IY0`a[", "c6d6ced902fe90f039f168837f7ce3d313df040e071281317fc6781a60cac2bc"),
+        User("q", "?!#", "TtVaKT?vkBlIrtbChI72yef7iWkLxkw4", "50bba7d209a17a3c36a3df151276d233ca868bf3b518165a6510b8e8c0bc2b7a"),
+        User("abcdefghij", "qwerty", "nUMXGQvmro8b5;AX7dLpwS_A4L`;RH^_", "2c06d373cd2549c31d8c1758daaa7773a8b905e32eb430f566c4c391827db121"))
 val permissions = listOf(Permission("A", "READ", users[0]), Permission("A.B.C", "WRITE", users[0]),
         Permission("A.B", "EXECUTE", users[1]), Permission("A", "READ", users[1]),
         Permission("A.B", "WRITE", users[1]), Permission("A.B.C", "READ", users[1]),
