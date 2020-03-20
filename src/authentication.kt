58957f03d1010076a31d3694874141c7f45e7cae
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-20 21:19:10 +0700

    Completed plan item 11.3

--- src/authentication.kt
+++ src/authentication.kt
@@ -6,6 +6,10 @@ fun loginExists(login: String) = users.any { it.login == login }
 
 fun authenticate(login: String, pass: String) = users.any { it.login == login && it.pass == pass }
 
+fun authHash(login: String, pass: String): Boolean {
+    return login == "vasya" && getSaltedHash(pass, getSalt("vasya")) == "fc5e540b95138a20e304971df53d66887495a05050031881799feeedcfa52f24"
+}
+
 fun getSalt(login: String) = "46c61ea8b02362b462f24f1ac8d9aacd"
 
 fun getSaltedHash(pass: String, salt: String) = hash(pass + salt)
