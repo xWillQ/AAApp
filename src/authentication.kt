f3d4f80e8076bd497a30ff24724c819962745e94
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-20 21:14:37 +0700

    Completed plan item 11.1

--- src/authentication.kt
+++ src/authentication.kt
@@ -3,3 +3,5 @@ fun validateLogin(login: String) = login.matches(Regex("[a-z]{1,10}"))
 fun loginExists(login: String) = users.any { it.login == login }
 
 fun authenticate(login: String, pass: String) = users.any { it.login == login && it.pass == pass }
+
+fun getSalt(login: String) = "46c61ea8b02362b462f24f1ac8d9aacd"
