9aea7351a08b7b8d49fd43bc4a1060c81fa49fb0
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-20 16:12:34 +0700

    Completed plan item 9.3

--- src/main.kt
+++ src/main.kt
@@ -47,6 +47,10 @@ fun hasPermission(res: String, role: String, user: String): Boolean {
 
 fun validateVol(vol: Int) = vol > 0
 
+fun validateDate(date: String): Boolean {
+    return date.matches(Regex("\\d{4}-((((0[13578])|(1[02]))-(0[1-9]|[12][0-9]|3[01]))|(((0[469])|(11))-(0[1-9]|[12][0-9]|(30)))|(02-(0[1-9]|[12][0-9])))"))
+}
+
 fun main(args: Array<String>) {
     val handler = ArgHandler(args)
     if (!handler.isArgs() || handler.needHelp()) {
