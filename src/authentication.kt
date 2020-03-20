cafef6a9476fb6f971bf78bc9c957cd38db004c5
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-20 21:16:56 +0700

    Completed plan item 11.2

--- src/authentication.kt
+++ src/authentication.kt
@@ -1,3 +1,5 @@
+import java.security.MessageDigest
+
 fun validateLogin(login: String) = login.matches(Regex("[a-z]{1,10}"))
 
 fun loginExists(login: String) = users.any { it.login == login }
@@ -5,3 +7,12 @@ fun loginExists(login: String) = users.any { it.login == login }
 fun authenticate(login: String, pass: String) = users.any { it.login == login && it.pass == pass }
 
 fun getSalt(login: String) = "46c61ea8b02362b462f24f1ac8d9aacd"
+
+fun getSaltedHash(pass: String, salt: String) = hash(pass + salt)
+
+fun hash(str: String): String {
+    val bytes = str.toByteArray()
+    val md = MessageDigest.getInstance("SHA-256")
+    val digest = md.digest(bytes)
+    return digest.fold("", { s, it -> s + "%02x".format(it) })
+}
