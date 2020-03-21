c83f31a343cc37e04a83e5addf71520329fbc879
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-22 00:13:14 +0700

    Created build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Create empty folder for temporary files
+rm -r temp/ > /dev/null 2>&1 # Delete folder if already exists, or supress error message if doesn't
+mkdir temp/
+
+# Compile programm with kotlin runtime
+kotlinc -include-runtime -cp lib/kotlinx-cli-0.2.1.jar -d temp/temp.jar src/
+cp lib/kotlinx-cli-0.2.1.jar temp/
+
+cd temp/
+
+# Unpack programm and dependencies
+jar -xf kotlinx-cli-0.2.1.jar
+jar -xf temp.jar
+rm kotlinx-cli-0.2.1.jar temp.jar # Remove unnecessary files to make final jar lighter
+
+# Merge programm with dependencies
+jar -cfe app.jar MainKt .
+
+cd ..
+mv temp/app.jar app.jar
+
+rm -r temp/
