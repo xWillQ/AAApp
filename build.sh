9bd746692864537fb0bfd213f27d2d40c2ec7a58
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-24 23:22:04 +0700

    Fixed spelling mistakes

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Create empty folder for temporary files
-rm -r temp/ > /dev/null 2>&1 # Delete folder if already exists, or supress error message if doesn't
+rm -r temp/ > /dev/null 2>&1 # Delete folder if already exists, or suppress error message if doesn't
 mkdir temp/
 
-# Compile programm with kotlin runtime
+# Compile program with kotlin runtime
 kotlinc -include-runtime -cp lib/kotlinx-cli-0.2.1.jar -d temp/temp.jar src/
 cp lib/kotlinx-cli-0.2.1.jar temp/
 
 cd temp/
 
-# Unpack programm and dependencies
+# Unpack program and dependencies
 jar -xf kotlinx-cli-0.2.1.jar
 jar -xf temp.jar
 rm kotlinx-cli-0.2.1.jar temp.jar # Remove unnecessary files to make final jar lighter
 
-# Merge programm with dependencies
+# Merge program with dependencies
 jar -cfe app.jar com.kafedra.bd.MainKt .
 
 cd ..
