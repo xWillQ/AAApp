6345ba923cab6963f2be456c4ed6bc0e1d0ad327
xWillQ <pasha.shevchenko.90@gmail.com>
2020-03-24 22:59:34 +0700

    Refactored tests, added return value

--- test.sh
+++ test.sh
@@ -2,6 +2,13 @@
 success=0
 total=0
 
+test() {
+    echo " $1:"
+    echo "  $2"
+    $($2 > /dev/null)
+    return $?
+}
+
 printResult() {
   echo "  expected: $2, actual: $1"
   if [ $1 -eq $2 ]; then
@@ -13,29 +20,26 @@ printResult() {
   fi
 }
 
+testAndPrint() {  # usage testAndPrint "test_number" "full_command" expected_return_value
+  test "$1" "$2" $3
+  printResult $? $3
+  return $?
+}
+
 echo
 echo "Help" # ==========================================================
 echo
 
-echo " T1.1:"
-echo "  java -jar app.jar "
-java -jar app.jar > /dev/null
-printResult $? 1                # 1 - expected return value
+testAndPrint "T1.1" "java -jar app.jar" 1
 success=$(( $success + $?))
 total=$(( $total + 1))
 
 
-echo " T1.2:"
-echo "  java -jar app.jar -h "
-java -jar app.jar -h > /dev/null
-printResult $? 0
+testAndPrint "T1.2" "java -jar app.jar -h" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T1.3:"
-echo "  java -jar app.jar -q  "
-java -jar app.jar -q  > /dev/null
-printResult $? 1
+testAndPrint "T1.3" "java -jar app.jar -q" 1
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
@@ -43,45 +47,27 @@ echo
 echo "Authentication" # =====================================================
 echo
 
-echo " T2.1:"
-echo "  java -jar app.jar -login vasya -pass 123 "
-java -jar app.jar -login vasya -pass 123 > /dev/null
-printResult $? 0
+testAndPrint "T2.1" "java -jar app.jar -login vasya -pass 123" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T2.2:"
-echo "  java -jar app.jar -pass 123 -login vasya "
-java -jar app.jar -pass 123 -login vasya > /dev/null
-printResult $? 0
+testAndPrint "T2.2" "java -jar app.jar -pass 123 -login vasya" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T2.3:"
-echo "  java -jar app.jar -login VASYA -pass 123 "
-java -jar app.jar -login VASYA -pass 123 > /dev/null
-printResult $? 2
+testAndPrint "T2.3" "java -jar app.jar -login VASYA -pass 123" 2
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T2.4:"
-echo "  java -jar app.jar -login asd -pass 123 "
-java -jar app.jar -login asd -pass 123 > /dev/null
-printResult $? 3
+testAndPrint "T2.4" "java -jar app.jar -login asd -pass 123" 3
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T2.5:"
-echo "  java -jar app.jar -login admin -pass 1234 "
-java -jar app.jar -login admin -pass 1234 > /dev/null
-printResult $? 4
+testAndPrint "T2.5" "java -jar app.jar -login admin -pass 1234" 4
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T2.6:"
-echo "  java -jar app.jar -login admin -pass admin "
-java -jar app.jar -login admin -pass admin > /dev/null
-printResult $? 0
+testAndPrint "T2.6" "java -jar app.jar -login admin -pass admin" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
@@ -89,73 +75,43 @@ echo
 echo "Authorization" # =====================================================
 echo
 
-echo " T3.1:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A "
-java -jar app.jar -login vasya -pass 123 -role READ -res A > /dev/null
-printResult $? 0
+testAndPrint "T3.1" "java -jar app.jar -login vasya -pass 123 -role READ -res A" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.2:"
-echo "  java -jar app.jar -login vasya -pass 123 -role DELETE -res A "
-java -jar app.jar -login vasya -pass 123 -role DELETE -res A > /dev/null
-printResult $? 5
+testAndPrint "T3.2" "java -jar app.jar -login vasya -pass 123 -role DELETE -res A" 5
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.3:"
-echo "  java -jar app.jar -login vasya -pass 123 -role WRITE -res A "
-java -jar app.jar -login vasya -pass 123 -role WRITE -res A > /dev/null
-printResult $? 6
+testAndPrint "T3.3" "java -jar app.jar -login vasya -pass 123 -role WRITE -res A" 6
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.4:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A.B "
-java -jar app.jar -login vasya -pass 123 -role READ -res A.B > /dev/null
-printResult $? 0
+testAndPrint "T3.4" "java -jar app.jar -login vasya -pass 123 -role READ -res A.B" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.5:"
-echo "  java -jar app.jar -login admin -pass admin -role WRITE -res A.B.C "
-java -jar app.jar -login admin -pass admin -role WRITE -res A.B.C > /dev/null
-printResult $? 0
+testAndPrint "T3.5" "java -jar app.jar -login admin -pass admin -role WRITE -res A.B.C" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.6:"
-echo "  java -jar app.jar -login vasya -pass 1234 -role DELETE -res A "
-java -jar app.jar -login vasya -pass 1234 -role DELETE -res A > /dev/null
-printResult $? 4
+testAndPrint "T3.6" "java -jar app.jar -login vasya -pass 1234 -role DELETE -res A" 4
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.7:"
-echo "  java -jar app.jar -login vasya -pass 123 -role WRITE -res A.B.C "
-java -jar app.jar -login vasya -pass 123 -role WRITE -res A.B.C > /dev/null
-printResult $? 0
+testAndPrint "T3.7" "java -jar app.jar -login vasya -pass 123 -role WRITE -res A.B.C" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.8:"
-echo "  java -jar app.jar -login admin -pass admin -role READ  "
-java -jar app.jar -login admin -pass admin -role READ  > /dev/null #удачная аутентификация
-printResult $? 0
+testAndPrint "T3.8" "java -jar app.jar -login admin -pass admin -role READ" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.9:"
-echo "  java -jar app.jar -login admin -pass admin -role EXECUTE -res A "
-java -jar app.jar -login admin -pass admin -role EXECUTE -res A > /dev/null
-printResult $? 6
+testAndPrint "T3.9" "java -jar app.jar -login admin -pass admin -role EXECUTE -res A" 6
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T3.10:"
-echo "  java -jar app.jar -login admin -pass admin -role WRITE -res A.A "
-java -jar app.jar -login admin -pass admin -role WRITE -res A.A > /dev/null
-printResult $? 6
+testAndPrint "T3.10" "java -jar app.jar -login admin -pass admin -role WRITE -res A.A" 6
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
@@ -163,84 +119,57 @@ echo
 echo "Accounting" # =====================================================
 echo
 
-echo " T4.1:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol 1024 "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol 1024 > /dev/null
-printResult $? 0
+testAndPrint "T4.1" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol 1024" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.2:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 20202-03-10 -de 2020-04-01 -vol 1024  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 20202-03-10 -de 2020-04-01 -vol 1024  > /dev/null #неверный год
-printResult $? 7
+testAndPrint "T4.2" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 20202-03-10 -de 2020-04-01 -vol 1024" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.3:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-12-10 -de 2020-13-01 -vol 1024  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-12-10 -de 2020-13-01 -vol 1024  > /dev/null #несуществующий месяц
-printResult $? 7
+testAndPrint "T4.3" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-12-10 -de 2020-13-01 -vol 1024" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.4:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-04-31 -de 2020-04-01 -vol 1024  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-04-31 -de 2020-04-01 -vol 1024  > /dev/null #день которого нет в месяце
-printResult $? 7
+testAndPrint "T4.4" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-04-31 -de 2020-04-01 -vol 1024" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.5:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-02-32 -de 2020-04-01 -vol 1024  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-02-32 -de 2020-04-01 -vol 1024  > /dev/null #несуществующее число
-printResult $? 7
+testAndPrint "T4.5" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-02-32 -de 2020-04-01 -vol 1024" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.6:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol -1024  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol -1024  > /dev/null #неверный объем
-printResult $? 7
+testAndPrint "T4.6" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol -1024" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.7:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol alot  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol alot  > /dev/null #некорректный объем
-printResult $? 7
+testAndPrint "T4.7" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01 -vol alot" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.8:"
-echo "  java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01  "
-java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01  > /dev/null #удачная авторизация
-printResult $? 0
+testAndPrint "T4.8" "java -jar app.jar -login vasya -pass 123 -role READ -res A -ds 2020-03-10 -de 2020-04-01" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.9:"
-echo "  java -jar app.jar -login admin -pass admin -role WRITE -res A.B.C -ds 2020-03-10 -de 2020-01-01 -vol 1024  "
-java -jar app.jar -login admin -pass admin -role WRITE -res A.B.C -ds 2020-03-10 -de 2020-01-01 -vol 1024  > /dev/null #проверка другого юзера
-printResult $? 0
+testAndPrint "T4.9" "java -jar app.jar -login admin -pass admin -role WRITE -res A.B.C -ds 2020-03-10 -de 2020-01-01 -vol 1024" 0
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.10:"
-echo "  java -jar app.jar -login vasya -pass 123 -role WRITE -res A.B.C -ds 2020-12-01 -de 2020-01-45 -vol 1024  "
-java -jar app.jar -login vasya -pass 123 -role WRITE -res A.B.C -ds 2020-12-01 -de 2020-01-45 -vol 1024  > /dev/null #не проходит авторизация
-printResult $? 7
+testAndPrint "T4.10" "java -jar app.jar -login vasya -pass 123 -role WRITE -res A.B.C -ds 2020-12-01 -de 2020-01-45 -vol 1024" 7
 success=$(( $success + $? ))
 total=$(( $total + 1))
 
-echo " T4.11:"
-echo "  java -jar app.jar -login vasya -pass 123 -role EXECUTE -res A.B.C -ds 2020-12-01 -de 2020-01-45 -vol 1024  "
-java -jar app.jar -login vasya -pass 123 -role EXECUTE -res A.B.C -ds 2020-12-01 -de 2020-01-45 -vol 1024  > /dev/null #не проходит авторизация
-printResult $? 6
+testAndPrint "T4.11" "java -jar app.jar -login vasya -pass 123 -role EXECUTE -res A.B.C -ds 2020-12-01 -de 2020-01-45 -vol 1024" 6
 success=$(( $success + $? ))
 total=$(( $total + 1))
+
+
 
 echo
-echo "Results:" # ==============================================================
-echo " Passed: $success"
-echo " Total: $total"
+if [ $success -eq $total ]; then
+  echo "Results: \033[32m$success/$total\033[0m tests passed"
+  return 0
+else
+  echo "Results: \033[31m$success/$total\033[0m tests passed"
+  return 1
+fi
