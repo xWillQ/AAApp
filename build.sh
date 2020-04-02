d1f639ffc0664ebedf486da7640e6d4d9b93a996
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-02 16:21:10 +0700

    Added and configured log4j

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-kotlinc -include-runtime -cp lib/kotlinx-cli-0.2.1.jar -d app.jar src/
+kotlinc -include-runtime -cp "lib/kotlinx-cli-0.2.1.jar:lib/apache-log4j-2.13.1-bin/log4j-api-2.13.1.jar:lib/apache-log4j-2.13.1-bin/log4j-core-2.13.1.jar" -d app.jar src/
