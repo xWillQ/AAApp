acf5625167e88fef71350d80bca2fda2d01cb61a
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-03 11:06:22 +0700

    Added database settings to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 export LOG4J_CONFIGURATION_FILE="$PWD/log4j2-test.xml"
+export H2_URL=jdbc:h2:./aaa
+export H2_LOGIN=se
+export H2_PASS=
+
 java --class-path "lib/kotlinx-cli-0.2.1.jar:lib/apache-log4j-2.13.1-bin/log4j-api-2.13.1.jar:lib/apache-log4j-2.13.1-bin/log4j-core-2.13.1.jar:app.jar:lib/h2-1.4.200.jar" com.kafedra.bd.MainKt "$@"
