3816ad6316136831ae903c7feb02e10e9bcb5e24
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-09 22:11:26 +0700

    Updated build and run scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-kotlinc -include-runtime -d app.jar src/ -cp "\
-lib/kotlinx-cli-0.2.1.jar:\
-lib/apache-log4j-2.13.1-bin/log4j-api-2.13.1.jar:\
-lib/apache-log4j-2.13.1-bin/log4j-core-2.13.1.jar:\
-lib/h2-1.4.200.jar:\
-lib/flyway-core-6.3.2.jar"
+./gradlew shadowJar
--- run.sh
+++ run.sh
@@ -11,9 +11,4 @@ if [ -z "$H2_PASS" ]; then
     export H2_PASS=
 fi
 
-java --class-path "app.jar:\
-lib/apache-log4j-2.13.1-bin/log4j-api-2.13.1.jar:\
-lib/apache-log4j-2.13.1-bin/log4j-core-2.13.1.jar:\
-lib/kotlinx-cli-0.2.1.jar:\
-lib/h2-1.4.200.jar:\
-lib/flyway-core-6.3.2.jar" com.kafedra.aaapp.MainKt "$@"
+java -jar AAApp.jar "$@"
