5287f8ff0edc0304b66cdda54eb4cb922b7e2317
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-10 13:26:04 +0700

    Created example test

---
+++ src/test/kotlin/example.kt
@@ -0,0 +1,28 @@
+import org.spekframework.spek2.Spek
+
+object MyTest: Spek({
+    beforeGroup {
+        println("before root")
+    }
+
+    group("some group") {
+        beforeEachTest {
+            println("before each test")
+        }
+        test("some test") {
+            println("some test")
+        }
+
+        test("another test") {
+            println("another test")
+        }
+
+        afterEachTest {
+            println("after each test")
+        }
+    }
+
+    afterGroup {
+        println("after root")
+    }
+})
