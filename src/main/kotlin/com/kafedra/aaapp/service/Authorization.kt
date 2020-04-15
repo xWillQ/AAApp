de43493d621ab61b408eb241f99cc326a46fb6b0
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-15 15:23:25 +0700

    Fixed formatting

--- src/main/kotlin/com/kafedra/aaapp/service/Authorization.kt
+++ src/main/kotlin/com/kafedra/aaapp/service/Authorization.kt
@@ -3,7 +3,7 @@ package com.kafedra.aaapp.service
 import com.kafedra.aaapp.Role
 import com.kafedra.aaapp.domain.DBWrapper
 
-class Authorization(private val dbWrapper: DBWrapper){
+class Authorization(private val dbWrapper: DBWrapper) {
 
     fun validateRole(role: String) = Role.values().any { it.role == role }
 
