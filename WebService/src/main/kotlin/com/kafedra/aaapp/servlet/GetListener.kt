cd14e02f36d89a10b1d753821b71d9ce5fde32ee
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-23 19:06:48 +0700

    Completed plan item 2.2

---
+++ WebService/src/main/kotlin/com/kafedra/aaapp/servlet/GetListener.kt
@@ -0,0 +1,15 @@
+package com.kafedra.aaapp.servlet
+
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "GetListener", urlPatterns = ["echo/get"], loadOnStartup = 1)
+class GetListener: HttpServlet() {
+    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
+        val query = request.queryString
+        val id = query.substringAfter("id=")
+        response.writer.print(id)
+    }
+}
