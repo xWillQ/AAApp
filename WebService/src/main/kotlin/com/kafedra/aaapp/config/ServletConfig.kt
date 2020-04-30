cc30c6e67d12167f8d2ccda21c41cb58983e5bfc
xWillQ <pasha.shevchenko.90@gmail.com>
2020-04-30 13:20:05 +0700

    Added ActivityServlet

--- WebService/src/main/kotlin/com/kafedra/aaapp/config/ServletConfig.kt
+++ WebService/src/main/kotlin/com/kafedra/aaapp/config/ServletConfig.kt
@@ -17,6 +17,7 @@ class ServletConfig() : GuiceServletContextListener() {
 
             serve("/ajax/user").with(UserServlet::class.java)
             serve("/ajax/authority").with(AuthorityServlet::class.java)
+            serve("/ajax/activity").with(ActivityServlet::class.java)
         }
     })
 }
