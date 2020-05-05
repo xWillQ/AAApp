c7273a827af7748ef03d424e22749bf3c9dee988
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-05 19:03:18 +0700

    Changed formatting of migrations

--- AuthCLI/src/main/resources/db/V1.2__Add_permissions.sql
+++ AuthCLI/src/main/resources/db/V1.2__Add_permissions.sql
@@ -1,8 +1,10 @@
-INSERT INTO permissions(res, role, login) VALUES ('A', 'READ', 'vasya');
-INSERT INTO permissions(res, role, login) VALUES ('A.B.C', 'WRITE', 'vasya');
-INSERT INTO permissions(res, role, login) VALUES ('A.B', 'EXECUTE', 'admin');
-INSERT INTO permissions(res, role, login) VALUES ('A', 'READ', 'admin');
-INSERT INTO permissions(res, role, login) VALUES ('A.B', 'WRITE', 'admin');
-INSERT INTO permissions(res, role, login) VALUES ('A.B.C', 'READ', 'admin');
-INSERT INTO permissions(res, role, login) VALUES ('B', 'EXECUTE', 'q');
-INSERT INTO permissions(res, role, login) VALUES ('A.A.A', 'EXECUTE', 'vasya');
+INSERT INTO permissions(res, role, login)
+VALUES
+    ('A', 'READ', 'vasya'),
+    ('A.B.C', 'WRITE', 'vasya'),
+    ('A.B', 'EXECUTE', 'admin'),
+    ('A', 'READ', 'admin'),
+    ('A.B', 'WRITE', 'admin'),
+    ('A.B.C', 'READ', 'admin'),
+    ('B', 'EXECUTE', 'q'),
+    ('A.A.A', 'EXECUTE', 'vasya');
