2193397616d36c3fa84f3a5e2ea114a6dbc52593
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-06 20:36:53 +0700

    Added Expose annotations to Authority

--- AuthCLI/src/main/kotlin/com/kafedra/aaapp/domain/Authority.kt
+++ AuthCLI/src/main/kotlin/com/kafedra/aaapp/domain/Authority.kt
@@ -1,5 +1,11 @@
 package com.kafedra.aaapp.domain
 
+import com.google.gson.annotations.Expose
 import com.kafedra.aaapp.Role
 
-data class Authority(val id: Int, val user: String, val role: Role, val res: String)
+data class Authority(
+        @Expose val id: Int,
+        val user: String,
+        @Expose val role: Role,
+        @Expose val res: String
+)
