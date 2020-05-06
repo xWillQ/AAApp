c130c50806f87c767fbc1781593aac095c28c9f5
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-06 20:38:13 +0700

    Created GSONProvider

---
+++ WebService/src/main/kotlin/com/kafedra/aaapp/di/GSONProvider.kt
@@ -0,0 +1,9 @@
+package com.kafedra.aaapp.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.inject.Provider
+
+class GSONProvider : Provider<Gson> {
+    override fun get() : Gson = GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+}
