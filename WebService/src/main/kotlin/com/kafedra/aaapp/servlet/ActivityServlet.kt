d6b6c5fb90a709d941896b109e3ac2f5bbf43b5d
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-07 14:53:57 +0700

    Completed ActivityServlet.service()

--- WebService/src/main/kotlin/com/kafedra/aaapp/servlet/ActivityServlet.kt
+++ WebService/src/main/kotlin/com/kafedra/aaapp/servlet/ActivityServlet.kt
@@ -22,6 +22,32 @@ class ActivityServlet: HttpServlet() {
 
     override fun service(request: HttpServletRequest, response: HttpServletResponse) {
         logger.info("Handling /ajax/user")
-        response.writer.print("Activity")
+
+        val id = request.getParameter("id")?.toIntOrNull()
+        val authorityId = request.getParameter("authorityId")?.toIntOrNull()
+
+        val activityList = when {
+            id != null -> {
+                logger.info("id = $id")
+                logger.info("Getting activities from database")
+                wrapper.getActivity(id)
+            }
+            authorityId != null -> {
+                logger.info("authorityId = $authorityId")
+                logger.info("Getting activities from database")
+                wrapper.getActivityByAuthority(authorityId)
+            }
+            else -> {
+                logger.info("id and authorityId are not specified, returning all activities")
+                logger.info("Getting activities from database")
+                wrapper.getActivity(0)
+            }
+        }
+
+        logger.info("Converting activities to json")
+        val gson = gsonProvider.get()
+        val json = gson.toJson(activityList)
+
+        response.writer.print(json)
     }
 }
