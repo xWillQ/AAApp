556a1a312637778849b89464b2259128eb98b6fe
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-12 21:54:09 +0700

    Injected HibernateProvider into Dao classes

--- AuthCLI/src/main/kotlin/com/kafedra/aaapp/dao/AuthorityDao.kt
+++ AuthCLI/src/main/kotlin/com/kafedra/aaapp/dao/AuthorityDao.kt
@@ -1,4 +1,8 @@
 package com.kafedra.aaapp.dao
 
+import com.google.inject.Inject
+import com.kafedra.aaapp.di.HibernateProvider
+
 class AuthorityDao {
+    @Inject lateinit var sessionProvider : HibernateProvider
 }
