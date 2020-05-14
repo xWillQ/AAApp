6c1e8ed676030690300a2bdfbd497c700745d400
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-14 14:00:11 +0700

    Modified Activity to work with hibernate

--- AuthCLI/src/main/kotlin/com/kafedra/aaapp/domain/Activity.kt
+++ AuthCLI/src/main/kotlin/com/kafedra/aaapp/domain/Activity.kt
@@ -3,13 +3,13 @@ package com.kafedra.aaapp.domain
 import com.google.gson.annotations.Expose
 import com.kafedra.aaapp.Role
 
-data class Activity(
-    @Expose val id: Int,
-    val user: User,
-    val authorityId: Int,
-    val res: String,
-    val role: Role,
-    @Expose val ds: String,
-    @Expose val de: String,
-    @Expose val vol: Int
-)
+class Activity {
+    @Expose var id: Int = 0
+    var user: User? = null
+    var authority: Authority? = null
+    var res: String = ""
+    var role: Role? = null
+    @Expose var ds: String = ""
+    @Expose var de: String = ""
+    @Expose var vol: Int = 0
+}
