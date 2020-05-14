3bafd37ccb79c844e36f40003ce0bdce03a8a263
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-14 20:35:42 +0700

    Added log message

--- AuthCLI/src/main/kotlin/com/kafedra/aaapp/di/HibernateProvider.kt
+++ AuthCLI/src/main/kotlin/com/kafedra/aaapp/di/HibernateProvider.kt
@@ -2,17 +2,21 @@ package com.kafedra.aaapp.di
 
 import com.google.inject.Provider
 import com.google.inject.Singleton
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.hibernate.SessionFactory
 import org.hibernate.cfg.Configuration
 
 @Singleton
 class HibernateProvider : Provider<SessionFactory> {
     private var sessionFactory: SessionFactory
+    val logger: Logger = LogManager.getLogger()
 
     init {
         val cfg = Configuration().configure()
         val url = System.getenv("JDBC_DATABASE_URL")
         if (url != null) {
+            logger.info("Reconfiguring hibernate to use postgres")
             cfg.setProperty("connection.url", url)
             cfg.setProperty("connection.username", System.getenv("JDBC_DATABASE_USERNAME"))
             cfg.setProperty("connection.password", System.getenv("JDBC_DATABASE_PASSWORD"))
