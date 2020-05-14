3b6e325b23db84e63bbdc7c7fd5f21e4d3bec710
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-14 20:28:45 +0700

    Updated migrations to work with postgresql

--- AuthCLI/src/main/resources/db/V1.0__Add_initial_tables.sql
+++ AuthCLI/src/main/resources/db/V1.0__Add_initial_tables.sql
@@ -1,19 +1,19 @@
 CREATE TABLE users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     login VARCHAR(10),
     hash VARCHAR(64),
     salt VARCHAR(32)
 );
 
 CREATE TABLE authorities(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     res VARCHAR(255),
     role VARCHAR(7),
     userId INT REFERENCES users (id)
 );
 
 CREATE TABLE activities(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY,
     userId INT REFERENCES users (id),
     authorityId INT REFERENCES authorities (id),
     res VARCHAR(255),
--- AuthCLI/src/main/resources/db/V1.2__Add_authorities.sql
+++ AuthCLI/src/main/resources/db/V1.2__Add_authorities.sql
@@ -1,10 +1,10 @@
-INSERT INTO authorities(res, role, userId)
+INSERT INTO authorities
 VALUES
-    ('A', 'READ', 1),
-    ('A.B.C', 'WRITE', 1),
-    ('A.B', 'EXECUTE', 2),
-    ('A', 'READ', 2),
-    ('A.B', 'WRITE', 2),
-    ('A.B.C', 'READ', 2),
-    ('B', 'EXECUTE', 3),
-    ('A.A.A', 'EXECUTE', 1);
+    (1, 'A', 'READ', 1),
+    (2, 'A.B.C', 'WRITE', 1),
+    (3, 'A.B', 'EXECUTE', 2),
+    (4, 'A', 'READ', 2),
+    (5, 'A.B', 'WRITE', 2),
+    (6, 'A.B.C', 'READ', 2),
+    (7, 'B', 'EXECUTE', 3),
+    (8, 'A.A.A', 'EXECUTE', 1);
