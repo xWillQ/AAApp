af4d7a10512e00a5cf2694a0c13ba7072a32f056
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-14 12:48:49 +0700

    Fixed bug in UserDao.getUser(id)
    UserDao.getUser(id) was returning list with null if called with non-existent id, now it returns empty list.

--- AuthCLI/src/main/kotlin/com/kafedra/aaapp/dao/UserDao.kt
+++ AuthCLI/src/main/kotlin/com/kafedra/aaapp/dao/UserDao.kt
@@ -2,11 +2,10 @@ package com.kafedra.aaapp.dao
 
 import com.google.inject.Inject
 import com.kafedra.aaapp.di.HibernateProvider
+import com.kafedra.aaapp.domain.Authority
 import com.kafedra.aaapp.domain.User
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import java.sql.Connection
-import kotlin.math.log
 
 class UserDao {
     @Inject lateinit var sessionProvider : HibernateProvider
@@ -33,16 +32,26 @@ class UserDao {
     fun getUser(id: Int) : List<User> {
         logger.info("Opening hibernate session")
         val session = sessionProvider.get().openSession()
+
         val userList = if (id != 0) {
             logger.info("Querying user with id = $id")
-            listOf(session.get(User::class.java, id))
+            val user = session.get(User::class.java, id)
+            if (user != null) {
+                logger.info("User exists, returning requested user")
+                listOf(user)
+            } else {
+                logger.info("User doesn't exists, returning empty list")
+                listOf()
+            }
         } else {
             logger.info("Id = 0, querying all users")
             val query = session.createQuery("FROM User", User::class.java)
             query.resultList
         }
+
         logger.info("Closing hibernate session")
         session.close()
+
         return userList
     }
 }
