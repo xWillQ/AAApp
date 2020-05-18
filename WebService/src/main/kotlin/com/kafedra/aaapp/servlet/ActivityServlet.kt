4fdfe3003d43fcd6885258645c0a84c97bc15161
xWillQ <pasha.shevchenko.90@gmail.com>
2020-05-18 21:45:32 +0700

    Completed ActivityServlet.doPost

--- WebService/src/main/kotlin/com/kafedra/aaapp/servlet/ActivityServlet.kt
+++ WebService/src/main/kotlin/com/kafedra/aaapp/servlet/ActivityServlet.kt
@@ -1,7 +1,9 @@
 package com.kafedra.aaapp.servlet
 
+import com.google.gson.reflect.TypeToken
 import com.google.inject.Inject
 import com.google.inject.Singleton
+import com.kafedra.aaapp.App
 import com.kafedra.aaapp.dao.ActivityDao
 import com.kafedra.aaapp.di.GSONProvider
 import com.kafedra.aaapp.di.injector.InjectLogger
@@ -19,9 +21,40 @@ class ActivityServlet: HttpServlet() {
     @Inject
     lateinit var dao: ActivityDao
 
+    override fun doPost(request: HttpServletRequest, response: HttpServletResponse) {
+        logger.info("Handling POST request")
+        logger.info("Receiving json")
+        val json = request.reader.readLine()
+        logger.info("json = \"$json\"")
+        logger.info("Deserializing json")
+        val type = object : TypeToken<Map<String, String>>() {}.type
+        val gson = gsonProvider.get()
+        val map = gson.fromJson<Map<String, String>>(json, type)
+
+        logger.info("Converting argument map to array")
+        val argList = mutableListOf<String>()
+        for (pair in map) {
+            if (pair.value != "") {
+                argList.add("-${pair.key}")
+                argList.add(pair.value)
+            }
+        }
+
+        logger.info("Running application")
+        val app = App()
+        val exitCode = app.run(argList.toTypedArray())
+        logger.info("Application exited with code ${exitCode.code} (${exitCode.name}")
+        logger.info("Sending exit code as a response")
+        response.writer.write("${exitCode.code}")
+    }
 
     override fun service(request: HttpServletRequest, response: HttpServletResponse) {
-        logger.info("Handling /ajax/user")
+        logger.info("Handling /ajax/activity")
+
+        if (request.method == "POST") {
+            this.doPost(request, response)
+            return
+        }
 
         val id = request.getParameter("id")?.toIntOrNull()
         val authorityId = request.getParameter("authorityId")?.toIntOrNull()
